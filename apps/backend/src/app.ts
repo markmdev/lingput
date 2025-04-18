bdc3647ec24e80752af447935d98de97f3a1beeb
mark-mdev <markmorgan.dev@gmail.com>
2025-04-18 07:33:17 -0500

    feat: add helmet middleware for security enhancements

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -18,9 +18,11 @@ import { authRouter } from "./modules/auth/authRoutes";
 import cookieParser from "cookie-parser";
 import { requestLogger } from "./middlewares/requestLogger";
 import { logger } from "./utils/logger";
+import helmet from "helmet";
 const app = express();
 const port = process.env.PORT;
 
+app.use(helmet());
 app.use(requestLogger);
 app.use(cookieParser());
 app.use(express.json());
