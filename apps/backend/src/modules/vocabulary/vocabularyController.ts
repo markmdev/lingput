bc16b5a49e909f2913485b7b8cad512298dcec57
mark-mdev <markmorgan.dev@gmail.com>
2025-04-18 21:32:13 -0500

    refactor: convert methods in VocabularyController to class properties

--- apps/backend/src/modules/vocabulary/vocabularyController.ts
+++ apps/backend/src/modules/vocabulary/vocabularyController.ts
@@ -6,7 +6,7 @@ import { z } from "zod";
 
 export class VocabularyController {
   constructor(private vocabularyService: VocabularyService) {}
-  async getAllWords(req: Request, res: Response) {
+  getAllWords = async (req: Request, res: Response) => {
     const { userId } = req.user;
     const { page, pageSize } = validateData(
       z.object({
@@ -17,41 +17,41 @@ export class VocabularyController {
     );
     const result = await this.vocabularyService.getWords(userId, page, pageSize);
     res.status(200).json(formatResponse(result.data, result.pagination));
-  }
+  };
 
-  async saveNewWord(req: Request, res: Response) {
+  saveNewWord = async (req: Request, res: Response) => {
     const { userId } = req.user;
     const { word, translation, article } = req.body;
     const newWord = await this.vocabularyService.saveNewWord({ word, translation, article, userId });
     res.status(201).json(formatResponse(newWord));
-  }
+  };
 
-  async saveManyWords(req: Request, res: Response) {
+  saveManyWords = async (req: Request, res: Response) => {
     const { userId } = req.user;
     const { words } = req.body;
     const savedWords = await this.vocabularyService.saveManyWords(words, userId);
     res.status(201).json(formatResponse(savedWords));
-  }
+  };
 
-  async deleteWord(req: Request, res: Response) {
+  deleteWord = async (req: Request, res: Response) => {
     const { userId } = req.user;
     const wordId = parseInt(req.params.id);
     await this.vocabularyService.deleteWord(wordId, userId);
     res.status(204).json(formatResponse({ status: "success" }));
-  }
+  };
 
-  async getWordById(req: Request, res: Response) {
+  getWordById = async (req: Request, res: Response) => {
     const { userId } = req.user;
     const wordId = parseInt(req.params.id);
     const word = await this.vocabularyService.getWordByID(wordId, userId);
     res.status(200).json(formatResponse(word));
-  }
+  };
 
-  async updateWord(req: Request, res: Response) {
+  updateWord = async (req: Request, res: Response) => {
     const { userId } = req.user;
     const { wordData } = req.body;
     const wordId = parseInt(req.params.id);
     const updatedWord = await this.vocabularyService.updateWord(wordId, userId, wordData);
     res.status(201).json(updatedWord);
-  }
+  };
 }
