02262c87687bf8f00494b3c34d8fee03adb5cb21
mark-mdev <markmorgan.dev@gmail.com>
2025-04-23 20:42:16 -0500

    refactor: convert methods to class properties in UnknownWordController

--- apps/backend/src/modules/unknownWord/unknownWordController.ts
+++ apps/backend/src/modules/unknownWord/unknownWordController.ts
@@ -6,23 +6,23 @@ import { formatResponse } from "@/middlewares/responseFormatter";
 
 export class UnknownWordController {
   constructor(private unknownWordService: UnknownWordService) {}
-  async markAsLearned(req: Request, res: Response) {
+  markAsLearned = async (req: Request, res: Response) => {
     const { wordId } = validateData(wordIdRequestSchema, req.params);
     const user = req.user;
     await this.unknownWordService.markAsLearned(wordId, user.userId);
     res.status(200).json(formatResponse({ message: "Word marked as learned" }));
-  }
+  };
 
-  async markAsLearning(req: Request, res: Response) {
+  markAsLearning = async (req: Request, res: Response) => {
     const { wordId } = validateData(wordIdRequestSchema, req.params);
     const user = req.user;
     await this.unknownWordService.markAsLearning(wordId, user.userId);
     res.status(200).json(formatResponse({ message: "Word marked as learning" }));
-  }
+  };
 
-  async getAllWords(req: Request, res: Response) {
+  getAllWords = async (req: Request, res: Response) => {
     const user = req.user;
     const words = await this.unknownWordService.getUnknownWords(user.userId);
     return res.status(200).json(formatResponse(words));
-  }
+  };
 }
