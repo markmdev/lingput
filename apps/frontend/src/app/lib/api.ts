6919252ab8c6227b621f44e7fd7f63b7f8f238a2
mark-mdev <markmorgan.dev@gmail.com>
2025-05-01 18:28:29 -0500

    feat: enhance API utility with improved error handling and new StoryApi class

--- apps/frontend/src/app/lib/api.ts
+++ apps/frontend/src/app/lib/api.ts
@@ -1,25 +1,69 @@
-export async function api(path: string, options: RequestInit) {
+import { ApiError } from "../types/ApiError";
+import { Story } from "../types/ApiObjects";
+import { ErrorResponse } from "../types/ErrorResponse";
+
+export async function api<T>({ path, options }: { path: string; options: RequestInit }): Promise<T | undefined> {
   const backendApiUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
   if (!backendApiUrl) {
     throw new Error("NEXT_PUBLIC_BACKEND_URL env variable is not set.");
   }
 
-  const res = await fetch(`${backendApiUrl}${path}`, {
-    cache: "no-store",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    credentials: "include",
-    ...options,
-  });
+  let res: Response;
+
+  try {
+    res = await fetch(`${backendApiUrl}${path}`, {
+      cache: "no-store",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      ...options,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(`Unexpected error while fetching ${path}: ${error.message}`);
+    }
+    throw new Error("Unknown error");
+  }
+
+  if (!res.ok) {
+    let errorPayload: ErrorResponse;
+
+    try {
+      errorPayload = await res.json();
+    } catch {
+      throw new ApiError(res.statusText, res.status);
+    }
+    throw new ApiError(errorPayload.error.message, errorPayload.error.code);
+  }
+
+  if (res.status === 204) {
+    return undefined;
+  }
 
   const json = await res.json();
-  return json;
+  return json.data;
 }
 
 export async function login({ email, password }: { email: string; password: string }) {
-  return api("/api/auth/login", {
-    method: "POST",
-    body: JSON.stringify({ email, password }),
+  return api({
+    path: "/api/auth/login",
+    options: {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+    },
   });
+}
+
+export class StoryApi {
+  constructor() {}
+
+  getAllStories(): Promise<Story[] | undefined> {
+    return api<Story[]>({
+      path: "/api/story",
+      options: {
+        method: "GET",
+      },
+    });
+  }
 }
