d0adb7001422e5e17f258c42920e26fbbe37fa14
mark-mdev <markmorgan.dev@gmail.com>
2025-05-01 18:29:20 -0500

    feat: implement story fetching and display in dashboard
    - Integrate SWR for data fetching from the API.
    - Display loading state and handle errors using ApiError.
    - Render StoryList component with fetched stories.

--- apps/frontend/src/app/(protected)/dashboard/page.tsx
+++ apps/frontend/src/app/(protected)/dashboard/page.tsx
@@ -1,3 +1,16 @@
+"use client";
+
+import StoryList from "@/app/components/StoryList";
+import { StoryApi } from "@/app/lib/api";
+import { ApiError } from "@/app/types/ApiError";
+import useSWR from "swr";
+
 export default function DashboardPage() {
-  return;
+  const storyApi = new StoryApi();
+  const { data, error, isLoading } = useSWR("/api/story", storyApi.getAllStories);
+
+  if (isLoading) return "Loading...";
+  if (error) return (error as ApiError).message;
+
+  return <StoryList storyList={data}></StoryList>;
 }
