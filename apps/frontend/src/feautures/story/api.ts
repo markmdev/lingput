96f65c0b5d352ff1e3206a6d7fd74e6c574442ea
mark-mdev <markmorgan.dev@gmail.com>
2025-05-02 18:32:09 -0500

    refactor: simplify API request handling in StoryApi
    - Introduce a generic fetch method to centralize API request logic.
    - Update getAllStories and getAllVocabulary methods to use the new fetch method for improved clarity and maintainability.

--- apps/frontend/src/feautures/story/api.ts
+++ apps/frontend/src/feautures/story/api.ts
@@ -5,21 +5,20 @@ import { ClientApi } from "@/lib/api.client";
 export class StoryApi {
   constructor(private clientApi: ClientApi) {}
 
-  getAllStories(): Promise<Story[]> {
-    return this.clientApi.api<Story[]>({
-      path: "/api/story",
+  fetch<T>(path: string): Promise<T> {
+    return this.clientApi.api<T>({
+      path,
       options: {
         method: "GET",
       },
     });
   }
 
+  getAllStories(): Promise<Story[]> {
+    return this.fetch<Story[]>("/api/story");
+  }
+
   getAllVocabulary(): Promise<VocabularyItem[]> {
-    return this.clientApi.api<VocabularyItem[]>({
-      path: "/api/vocab/allwords",
-      options: {
-        method: "GET",
-      },
-    });
+    return this.fetch<VocabularyItem[]>("/api/vocab/allwords");
   }
 }
