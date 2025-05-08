50e3075a9637fa822698dc7addd518768967d3c0
mark-mdev <markmorgan.dev@gmail.com>
2025-05-07 22:58:41 -0500

    refactor: rename UnprotectedLayout to ProtectedLayout for clarity

--- apps/frontend/src/app/(protected)/layout.tsx
+++ apps/frontend/src/app/(protected)/layout.tsx
@@ -1,7 +1,7 @@
 import { getCurrentUser } from "@/feautures/auth/api.server";
 import { redirect } from "next/navigation";
 
-export default async function UnprotectedLayout({ children }: { children: React.ReactNode }) {
+export default async function ProtectedLayout({ children }: { children: React.ReactNode }) {
   const user = await getCurrentUser();
   if (!user) redirect("/login");
 
