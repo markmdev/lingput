e1e3465e5d65f605490c28731eb7be0727687266
mark-mdev <markmorgan.dev@gmail.com>
2025-06-30 19:45:21 -0700

    style(login): enhance login form UI with improved layout and styling

--- apps/frontend/src/app/(unprotected)/login/page.tsx
+++ apps/frontend/src/app/(unprotected)/login/page.tsx
@@ -24,14 +24,37 @@ export default function LoginPage() {
   };
 
   return (
-    <div>
+    <div className="flex flex-col h-screen items-center justify-center">
       <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
-        <button type="submit">Login</button>
-        <p className="text-red-500">{error?.message}</p>
+        <div className="flex flex-col gap-2 justify-center bg-gray-100 py-10 px-20 rounded-lg shadow-xl border border-gray-200">
+          <label htmlFor="email" className="font-bold">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="py-2 px-4 bg-blue-50 rounded-lg border border-gray-300 text-gray-700"
+          ></input>
+          <label htmlFor="password" className="font-bold">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="py-2 px-4 bg-blue-50 rounded-lg border border-gray-300 text-gray-700"
+          ></input>
+          <button
+            type="submit"
+            className="bg-blue-500 py-2 px-10 w-fit text-xl font-semibold rounded-lg text-white self-center mt-4 cursor-pointer"
+          >
+            Login
+          </button>
+          <p className="text-red-500">{error?.message}</p>
+        </div>
       </form>
     </div>
   );
