672deed1000d660bd2ee242ddc1e97208d637f78
mark-mdev <markmorgan.dev@gmail.com>
2025-08-16 11:44:50 -0700

    feat(vocab-assessment): update layout and styling for improved user experience; enhance loading and ready states with additional messaging

--- apps/frontend/src/app/(protected)/vocab-assessment/page.tsx
+++ apps/frontend/src/app/(protected)/vocab-assessment/page.tsx
@@ -90,48 +90,61 @@ export default function VocabAssessmentPage() {
   };
 
   return (
-    <div className="p-6 bg-white/80 backdrop-blur-sm border border-slate-100 flex flex-col items-center rounded-2xl w-full md:w-2/3 lg:w-1/2 shadow-sm mx-auto">
-      <h1 className="text-xl font-semibold text-slate-900">Vocabulary Assessment</h1>
-      {status === "loading" && (
-        <div className="py-6">
-          <p className="text-slate-600">Loading...</p>
-        </div>
-      )}
-
-      {status === "ready" && (
-        <div className="py-4">
-          <StartButton onClick={handleStart} />
-        </div>
-      )}
-
-      {status === "started" && (
-        <div className="flex flex-col gap-4 items-center w-full">
-          <p className="text-slate-700">
-            Step {apiResponse?.step}{" "}
-            {apiResponse?.lastStep ? <span className="italic">(Last)</span> : ""}
-          </p>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 w-full">
-            {apiResponse?.wordsToReview?.map((item) => (
-              <Word key={item.id} word={item} answer={handleWordAnswer} status={answer[item.id]} />
-            ))}
+    <div className="fixed inset-0 z-40 flex items-center justify-center">
+      <div className="px-6 py-20 bg-white/80 backdrop-blur-sm border border-slate-100 flex flex-col items-center rounded-2xl w-full md:w-2/3 lg:w-1/2 shadow-sm mx-auto">
+        <h1 className="text-xl font-semibold text-slate-900">Vocabulary Assessment</h1>
+        {status === "loading" && (
+          <div className="py-6">
+            <p className="text-slate-600">Loading...</p>
           </div>
-          <ContinueButton
-            isActive={Object.keys(answer).length !== apiResponse?.wordsToReview?.length}
-            onClick={handleContinue}
-          />
-        </div>
-      )}
+        )}
 
-      {status === "completed" && (
-        <div className="py-6">
-          <p className="font-semibold text-lg text-slate-900">
-            Vocabulary size: {apiResponse?.vocabularySize}
-          </p>
-          <button className="py-2.5 px-6 text-lg font-semibold rounded-lg w-fit bg-emerald-500 cursor-pointer text-white hover:bg-emerald-600 transition-colors">
-            Continue
-          </button>
-        </div>
-      )}
+        {status === "ready" && (
+          <div className="py-4 flex flex-col items-center">
+            <p className="text-slate-700 text-center mt-2 mb-6 max-w-xl">
+              To provide you with the most effective and personalized stories, we first need to
+              understand your current vocabulary knowledge. This quick assessment helps us tailor
+              content to your level, ensuring you get the right amount of challenge and support as
+              you learn. Please answer honestly for the best experience!
+            </p>
+            <StartButton onClick={handleStart} />
+          </div>
+        )}
+
+        {status === "started" && (
+          <div className="flex flex-col gap-4 items-center w-full">
+            <p className="text-slate-700">
+              Step {apiResponse?.step}{" "}
+              {apiResponse?.lastStep ? <span className="italic">(Last)</span> : ""}
+            </p>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 w-full">
+              {apiResponse?.wordsToReview?.map((item) => (
+                <Word
+                  key={item.id}
+                  word={item}
+                  answer={handleWordAnswer}
+                  status={answer[item.id]}
+                />
+              ))}
+            </div>
+            <ContinueButton
+              isActive={Object.keys(answer).length !== apiResponse?.wordsToReview?.length}
+              onClick={handleContinue}
+            />
+          </div>
+        )}
+
+        {status === "completed" && (
+          <div className="py-6">
+            <p className="font-semibold text-lg text-slate-900">
+              Vocabulary size: {apiResponse?.vocabularySize}
+            </p>
+            <button className="py-2.5 px-6 text-lg font-semibold rounded-lg w-fit bg-emerald-500 cursor-pointer text-white hover:bg-emerald-600 transition-colors">
+              Continue
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- apps/frontend/src/features/vocabAssessment/components/StartButton.tsx
+++ apps/frontend/src/features/vocabAssessment/components/StartButton.tsx
@@ -1,7 +1,7 @@
 export default function StartButton({ onClick }: { onClick: () => void }) {
   return (
     <button
-      className="py-2 px-4 text-lg bg-emerald-500 font-semibold rounded-lg text-white hover:bg-emerald-600 transition-colors"
+      className="py-2 px-8 text-lg bg-emerald-500 font-semibold rounded-lg text-white hover:bg-emerald-600 transition-colors cursor-pointer"
       onClick={onClick}
     >
       Start
