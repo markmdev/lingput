fbc93d4f56e56927c5423645e175e95e519a6a1a
mark-mdev <markmorgan.dev@gmail.com>
2025-08-23 12:12:58 -0700

    feat(auth, story): enhance type safety and improve hooks
    This commit updates the `useAuthRedirect` and `useStories` hooks to utilize the `ApiError` type for better error handling. Additionally, the `useWordStatus` hook is refactored to use `useMemo` for instantiating API clients, optimizing performance. A new Dockerfile for the landing application is also introduced, streamlining the build and start process.

--- apps/frontend/src/features/story/hooks/useStories.ts
+++ apps/frontend/src/features/story/hooks/useStories.ts
@@ -1,8 +1,15 @@
 import { StoryApi } from "@/features/story/api";
 import { ClientApi } from "@/lib/ClientApi";
-import useSWR from "swr";
+import { ApiError } from "@/types/ApiError";
+import useSWR, { KeyedMutator } from "swr";
+import { Story } from "../types";
 
-export function useStories() {
+export function useStories(): {
+  stories: Story[] | undefined;
+  error: ApiError;
+  isLoading: boolean;
+  mutateStories: KeyedMutator<Story[]>;
+} {
   const clientApi = new ClientApi();
   const storyApi = new StoryApi(clientApi);
   const { data, error, isLoading, mutate } = useSWR("/api/story", () => storyApi.getAllStories());
